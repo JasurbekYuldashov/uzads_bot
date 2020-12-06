1df21658a15d616a812b72e3777a78114b918a54
Jasubek <57658965+JasurbekYuldashov@users.noreply.github.com>
2020-12-06 09:31:28 +0500

    actions,components,constants added

---
+++ index.js
@@ -0,0 +1,20 @@
+const compression = require("compression")
+const express = require("express")
+require("dotenv").config()
+
+
+const app = express()
+const port = 8080
+
+
+app.use(compression())
+
+app.get('/', (req, res) => {
+	res.send('Hello World!')
+	console.log(req)
+
+})
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`)
+})
